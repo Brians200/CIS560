7a5af6626628c6eb4b67175c2f527cf08db1dfbe
Brian Sweeney <brians200@gmail.com>
2011-10-27 20:31:15 -0500

    Eric viewStock intro

--- grails-app/controllers/cis560/StockController.groovy
+++ grails-app/controllers/cis560/StockController.groovy
@@ -8,8 +8,17 @@ class StockController {
 	
 	def viewStock={
 		
+		def stockSymbol
+		def stockExchange
+		if(chainModel!=null&&chainModel.Symbol!=null&&chainModel.Exchange!=null)
+		{
+			stockSymbol = chainModel.Symbol
+			stockExchange = chainModel.Exchange
+		}
 		
-		[]
+		
+		//model to return
+		[Symbol:stockSymbol, Exchange:stockExchange]
 	}
 	
 }
