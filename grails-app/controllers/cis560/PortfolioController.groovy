94dd2bc79fc410c4aa9b48c52e94690d8f1afa0c
Brian Sweeney <brians200@gmail.com>
2011-11-29 23:35:01 -0600

    Now displays a list of the portfolios a user has and links to the view. Also added the initial view for heath to fill out

--- grails-app/controllers/cis560/PortfolioController.groovy
+++ grails-app/controllers/cis560/PortfolioController.groovy
@@ -1,11 +1,36 @@
 package cis560
 
+import java.sql.ResultSet
+
 class PortfolioController {
 
 	//This is overview
-    def index = { }
+    def index = { 
+		SqlLogic.SetStatement("SELECT pname FROM Portfolios WHERE username = '$session.userName'")
+		ResultSet portfolios = SqlLogic.ExecuteQuery()
+		def portfolioList = []
+		while(portfolios.next())
+		{
+			portfolioList.add(portfolios.getString(1))
+		}
+		
+		portfolios.close();
+		
+		[portfolios:portfolioList]
+	}
 	
 	def delete = { }
+	
+	def singlePortfolio={
+		
+		// The portfolio they wanna view is in this variable: params.portfolioName
+		// The user's name is here: session.userName
+		
+		
+		//This is a mapping of what you want to pass to the GSP, so pass in things you need to display
+		[portfolioName:params.portfolioName]
+	}
+	
 	
 	def create = { }
 }
